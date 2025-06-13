6135a5d81c1706f7597aecc8bd350d0140805a97
AnthonyFerrerG <anthonydario999710@gmail.com>
2025-06-12 21:10:33 -0300

    DataSource.kt

---
+++ DataSource.kt
@@ -0,0 +1,13 @@
+package com.example.cupcake.data
+
+object DataSource {
+    val quantityOptions = listOf(1, 6, 12)
+    val flavors = listOf(
+        R.string.chocolate,
+        R.string.vanilla,
+        R.string.red_velvet,
+        R.string.lemon,
+        R.string.coffee,
+        R.string.caramel
+    )
+}
